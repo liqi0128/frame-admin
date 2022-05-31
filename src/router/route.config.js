b7f29cb60c208319a9971a6cd287396a58171010
lizuoqi <2801073245@qq.com>
2022-05-31 18:02:19 +0800

    路由

--- src/router/route.config.js
+++ src/router/route.config.js
@@ -1,6 +1,6 @@
 //路由配置文件
-import { lazy } from 'react'
-const layout = lazy(() => import('../layout/AppMain'))
+
+const layout = () => import('../layout/AppMain')
 
 let constantRouterMap = [
   {
@@ -11,34 +11,37 @@ let constantRouterMap = [
       {
         path: '/subset0',
         name:'subset0',
-        component: lazy(() => import('../pages/Subset')),
+        component: () => import('../pages/Subset'),
+        meta: {
+            title: '业务常识',
+        },
       },
       {
-        path: '/subset1',
+        path: '/subset1/:id',
         name:'subset1',
-        component: lazy(() => import('../pages/Subset1')),
+        component: () => import('../pages/Subset1'),
       },
       {
         path: '/subset2',
         name:'subset2',
-        component: lazy(() => import('../pages/Subset2')),
+        component: () => import('../pages/Subset2'),
       },
       {
         path: '/test',
         name:'test',
-        component: lazy(() => import('../pages/Test')),
+        component: () => import('../pages/Test'),
       },
       {
         path: '/App',
         name:'app',
-        component: lazy(() => import('../pages/App')),
+        component: () => import('../pages/App'),
       },
     ]
   },
   {
     path: '/app2',
     name:'main',
-    component: lazy(() => import('../pages/App'))
+    component: () => import('../pages/App')
   }
 ]
 //异步获取路由
